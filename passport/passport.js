d65534fdfd1cbcfaf64c0e4812ec9236a7d5858b
Chase Wickett <wickettc@yahoo.com>
2021-02-26 15:34:52 -0500

    updated formatting

--- passport/passport.js
+++ passport/passport.js
@@ -13,16 +13,16 @@ passport.use(
     new LocalStrategy((username, password, done) => {
         User.findOne({ username }, (err, user) => {
             if (err) return done(err);
-            if (!user) return done(null, false, {msg: 'Incorrect Email'});
+            if (!user) return done(null, false, { msg: 'Incorrect Email' });
             // need to check if password is correct here
             bcrypt.compare(password, user.password, (error, res) => {
                 if (res) {
                     // passwords match
-                    return done(null, user)
+                    return done(null, user);
                 }
                 // passwords do not match
-                return done(null, false, {msg: 'Incorrect Password'})
-            })
+                return done(null, false, { msg: 'Incorrect Password' });
+            });
         });
     })
 );
@@ -34,11 +34,12 @@ passport.use(
             secretOrKey: process.env.SECRET,
         },
 
-        async (token, done) =>{
+        async (token, done) => {
             try {
-                return done(null, token.user)
+                return done(null, token.user);
             } catch (err) {
-                done(err)
-            }}
+                done(err);
+            }
+        }
     )
 );
