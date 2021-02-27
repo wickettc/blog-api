b7cf47889d212a50a3093d8dbf48218599094b46
Chase Wickett <wickettc@yahoo.com>
2021-02-27 17:43:15 -0500

    prep for heroku deploy

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const compression = require('compression');
+const helmet = require('helmet');
 require('dotenv').config();
 
 require('./passport/passport');
@@ -12,11 +14,14 @@ const authRouter = require('./routes/auth');
 
 const app = express();
 
-const mongoDB = `mongodb+srv://admin:${process.env.MONGODB_PW}@cluster0.fefqf.mongodb.net/blog-api?retryWrites=true&w=majority`;
+const devDbUrl = `mongodb+srv://admin:${process.env.MONGODB_PW}@cluster0.fefqf.mongodb.net/blog-api?retryWrites=true&w=majority`;
+const mongoDB = process.env.MONGODB_URI || devDbUrl;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
+app.use(compression());
+app.use(helmet());
 app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
